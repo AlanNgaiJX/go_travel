94f8119d5ff92e06232f506c6bd1ea9f24dffcd3
WeiAlan <iVnalAGreen@WeiAlandeMacBook-Pro.local>
2019-07-17 03:01:25 +0800

    header static

--- src/main.js
+++ src/main.js
@@ -4,11 +4,19 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
+import fastclick from 'fastclick'
+
+// 移动端点击事件延迟解决方案
+fastclick.attach(document.body);
+
 Vue.config.productionTip = false
 
 // CSS
+  // 兼容预设
 import 'styles/reset.css';
+  // 移动端1px边框解决方案
 import 'styles/border.css';
+  // 启用iconfont
 import 'styles/iconfont.css';
 
 
--- src/pages/components/HomeHeader.vue
+++ src/pages/components/HomeHeader.vue
@@ -1,6 +1,14 @@
 <template>
-  <div class="">
-      homeHeader
+  <div class="header">
+      <div class="header_left">
+          <span class="icon">&#xe624;</span>
+      </div>
+      <div class="header_middle">
+          <span><span class="icon">&#xe632;</span>输入城市/景点/游玩主题</span>
+      </div>
+      <div class="header_right">
+          <span>北京<span class="icon">&#xe62d;</span></span>
+      </div>
   </div>
 </template>
 
@@ -15,6 +23,32 @@ export default {
  }
 </script>
 
-<style lang="" scoped>
- 
+<style lang="stylus" scoped>
+@import '~styles/varibles'
+
+    .header
+        line-height $headerHeight
+        display flex
+        background-color $bgColor
+        color white
+        .header_left
+            width .64rem
+            text-align center
+        .header_middle 
+            flex 1
+            height .64rem
+            line-height .64rem
+            margin .11rem .1rem 0 .1rem
+            padding 0 .1rem
+            background-color #ffffff
+            color $greyTextColor
+            border-radius .1rem
+            .icon
+                margin-right .1rem
+        .header_right
+            width 1.24rem
+            text-align center
+            font-size .24rem
+            .icon
+                margin-left .1rem            
 </style>
