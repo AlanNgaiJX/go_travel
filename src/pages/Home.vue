adf6e6c1658459692b8d6f900740e317950b4e55
WeiAlan <iVnalAGreen@WeiAlandeMacBook-Pro.local>
2019-07-16 22:33:40 +0800

    first stage

---
+++ src/pages/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="">
+      
+  </div>
+</template>
+
+<script>
+import HomeHeader from './components/HomeHeader'
+export default {
+  name: '',
+  components:{
+    HomeHeader
+  },
+  data() { 
+    return {
+
+    }
+  }
+ }
+</script>
+
+<style lang="" scoped>
+
+</style>
